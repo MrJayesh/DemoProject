503cb3ee1310e8378efe96f1b320d83ab6742bb5
Jayesh <jayesh.jspatil@gmail.com>
2023-01-17 21:12:14 +0530

    Added ArrayList Class

---
+++ src/com/DemoProject/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.DemoProject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Jay");
+		al.add("Nikhil");
+		al.add("Java");
+	}
+
+}
